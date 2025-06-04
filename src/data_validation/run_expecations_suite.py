7c5d47551c085584c6593b8b0cd09c90063c3b7e
dakshvanshaj <davevanshaj55646@gmail.com>
2025-06-04 12:18:11 +0530

    chore: replace load_batch with get_or_create_batch_definition and load_batch_from_definition
    Refactored code to use separate functions for batch definition creation/loading
    and batch loading for better modularity and clarity

--- src/data_validation/run_expecations_suite.py
+++ src/data_validation/run_expecations_suite.py
@@ -2,7 +2,8 @@ from load_data import (
     get_ge_context,
     get_or_create_datasource,
     get_or_create_csv_asset,
-    load_batch,
+    get_or_create_batch_definition,
+    load_batch_from_definition,
 )
 from expectations_suite import (
     get_or_create_expectation_suite,
@@ -22,8 +23,8 @@ def main():
     context = get_ge_context(project_root_dir=root_dir)
     data_source = get_or_create_datasource(context, source_name, base_dir)
     csv_asset = get_or_create_csv_asset(data_source, asset_name)
-    batch = load_batch(csv_asset, batch_name, batch_path)
-
+    batch_definition = get_or_create_batch_definition(csv_asset, batch_name, batch_path)
+    batch = load_batch_from_definition(batch_definition)
     suite = get_or_create_expectation_suite(context, suite_name)
 
     expect_column_max_to_be_between(suite, "price", 1, 2000)
