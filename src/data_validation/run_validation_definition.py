7008a96af0e28eb4fe9856135500d80fc57a5c9f
dakshvanshaj <davevanshaj55646@gmail.com>
2025-06-05 13:07:40 +0530

    chore: refactor code to use utils.py and configurations from config.py

--- src/data_validation/run_validation_definition.py
+++ src/data_validation/run_validation_definition.py
@@ -1,10 +1,3 @@
-from load_data import (
-    get_ge_context,
-    get_or_create_datasource,
-    get_or_create_csv_asset,
-    get_or_create_batch_definition,
-    load_batch_from_definition,
-)
 from expectations_suite import get_or_create_expectation_suite
 
 from validation_definition import (
@@ -12,33 +5,40 @@ from validation_definition import (
     run_validation,
 )
 
+from utils import initialize_ge_components
+from config import (
+    ROOT_DIR,
+    SOURCE_NAME,
+    BASE_DIR,
+    ASSET_NAME,
+    BATCH_NAME,
+    BATCH_PATH,
+    SUITE_NAME,
+    VALIDATION_DEFINITION_NAME,
+    BATCH_PARAMETERS,
+)
+
 
 def main():
-    root_dir = "./great_expectations"
-    source_name = "flights"
-    base_dir = "../../data"
-    asset_name = "flights_data"
-    batch_name = "flights_main"
-    batch_path = "flights.csv"
-    suite_name = "flights_expectations_suite"
-    validation_definition_name = "flights_validation_definition"
-    batch_parameters = {"path": "flights.csv"}
-    # Load GE context and data
-
-    context = get_ge_context(project_root_dir=root_dir)
-    data_source = get_or_create_datasource(context, source_name, base_dir)
-    csv_asset = get_or_create_csv_asset(data_source, asset_name)
-    batch_definition = get_or_create_batch_definition(csv_asset, batch_name, batch_path)
-    batch = load_batch_from_definition(batch_definition)
-    expectation_suite = get_or_create_expectation_suite(context, suite_name)
+    # Load data context, data, asset, batch def and batch from utils.py
+    context, data_source, csv_asset, batch_definition, batch = initialize_ge_components(
+        ROOT_DIR,
+        SOURCE_NAME,
+        BASE_DIR,
+        ASSET_NAME,
+        BATCH_NAME,
+        BATCH_PATH,
+    )
+    # Try to get existing expectation_suite
+    expectation_suite = get_or_create_expectation_suite(context, SUITE_NAME)
 
     # Try to get existing validation definition, create if not found
     validation_definition = get_or_create_validation_definition(
-        context, batch_definition, expectation_suite, validation_definition_name
+        context, batch_definition, expectation_suite, VALIDATION_DEFINITION_NAME
     )
 
     # Run validation with batch parameters
-    results = run_validation(validation_definition, batch_parameters)
+    results = run_validation(validation_definition, BATCH_PARAMETERS)
 
     # Print results
     print(results)
