ac3807fa418f749249ff22fec6c4cc93b5bd82d2
dakshvanshaj <davevanshaj55646@gmail.com>
2025-06-05 21:36:46 +0530

    chore: replaced print with logging and update path variable names

--- src/data_validation/run_expecations_suite.py
+++ src/data_validation/run_expecations_suite.py
@@ -2,24 +2,30 @@ from expectations_suite import (
     get_or_create_expectation_suite,
     expect_column_max_to_be_between,
 )
-from utils import initialize_ge_components
+from utils import setup_logger, initialize_ge_components
 from config import (
-    ROOT_DIR,
+    GE_ROOT_DIR,
     SOURCE_NAME,
-    BASE_DIR,
+    DATA_BASE_DIR,
     ASSET_NAME,
     BATCH_NAME,
     BATCH_PATH,
     SUITE_NAME,
+    SUITE_LOGS,
 )
+import logging
 
 
 def main():
+    # Setup logger basic configuration
+    setup_logger(verbose=False, log_file=SUITE_LOGS, mode="w")
+    logger = logging.getLogger(__name__)
+
     # Initialize GE components using config values
     context, data_source, csv_asset, batch_definition, batch = initialize_ge_components(
-        ROOT_DIR,
+        GE_ROOT_DIR,
         SOURCE_NAME,
-        BASE_DIR,
+        DATA_BASE_DIR,
         ASSET_NAME,
         BATCH_NAME,
         BATCH_PATH,
@@ -29,9 +35,11 @@ def main():
     suite = get_or_create_expectation_suite(context, SUITE_NAME)
 
     # Add expectation on 'price' column
+    # More will be added here
+    # we will also handle case when we may already hav expectation added
     expect_column_max_to_be_between(suite, "price", 1, 2000)
 
-    print(f"Expectation suite '{SUITE_NAME}' updated.")
+    logger.info(f"Expectation suite '{SUITE_NAME}' updated.")
 
 
 if __name__ == "__main__":
