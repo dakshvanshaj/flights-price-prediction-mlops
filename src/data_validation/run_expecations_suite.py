57943eeb4f0150cd9491b825c4619218ee9c9696
dakshvanshaj <davevanshaj55646@gmail.com>
2025-06-09 15:44:08 +0530

    Refactor: used upsert_expectation

--- src/data_validation/run_expecations_suite.py
+++ src/data_validation/run_expecations_suite.py
@@ -1,7 +1,6 @@
-from expectations_suite import (
-    get_or_create_expectation_suite,
-    expect_column_max_to_be_between,
-)
+import logging
+from expectations_suite import get_or_create_expectation_suite, upsert_expectation
+from expectations import price_range_expectation
 from utils import setup_logger, initialize_ge_components
 from config import (
     GE_ROOT_DIR,
@@ -13,33 +12,22 @@ from config import (
     SUITE_NAME,
     SUITE_LOGS,
 )
-import logging
 
 
 def main():
-    # Setup logger basic configuration
     setup_logger(verbose=False, log_file=SUITE_LOGS, mode="w")
     logger = logging.getLogger(__name__)
 
-    # Initialize GE components using config values
-    context, data_source, csv_asset, batch_definition, batch = initialize_ge_components(
-        GE_ROOT_DIR,
-        SOURCE_NAME,
-        DATA_BASE_DIR,
-        ASSET_NAME,
-        BATCH_NAME,
-        BATCH_PATH,
+    context, *_ = initialize_ge_components(
+        GE_ROOT_DIR, SOURCE_NAME, DATA_BASE_DIR, ASSET_NAME, BATCH_NAME, BATCH_PATH
     )
 
-    # Get or create expectation suite
     suite = get_or_create_expectation_suite(context, SUITE_NAME)
 
-    # Add expectation on 'price' column
-    # More will be added here
-    # we will also handle case when we may already hav expectation added
-    expect_column_max_to_be_between(suite, "price", 1, 1500)
+    # Add/Update expectations
+    upsert_expectation(suite, price_range_expectation())
 
-    logger.info(f"Expectation suite '{SUITE_NAME}' updated.")
+    logger.info(f"Expectation suite '{SUITE_NAME}' updated successfully.")
 
 
 if __name__ == "__main__":
