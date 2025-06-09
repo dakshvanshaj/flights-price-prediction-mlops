0ea599b486fc7e3ebcb665ec82cb23f47ffd71c7
dakshvanshaj <davevanshaj55646@gmail.com>
2025-06-09 22:09:18 +0530

    chore: remove id and add name in meta

--- src/data_validation/expectations.py
+++ src/data_validation/expectations.py
@@ -3,13 +3,10 @@ import great_expectations as gx
 
 def price_range_expectation():
     return gx.expectations.ExpectColumnMaxToBeBetween(
-        id="column_range",
         column="price",
         min_value=1,
-        max_value=1500,
+        max_value=500,
         strict_min=False,
         strict_max=False,
+        meta={"name": "price_range_expectation"},
     )
-
-
-# Add more expectation builder functions here
