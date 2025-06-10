a5b79c07a4d9fc12f6a90eb6c317dc55cd135955
dakshvanshaj <davevanshaj55646@gmail.com>
2025-06-10 18:02:42 +0530

    chore: change parameters for testing

--- src/data_validation/expectations.py
+++ src/data_validation/expectations.py
@@ -4,9 +4,10 @@ import great_expectations as gx
 def price_range_expectation():
     return gx.expectations.ExpectColumnMaxToBeBetween(
         column="price",
-        min_value=1,
-        max_value=500,
-        strict_min=False,
-        strict_max=False,
+        min_value=5,
+        max_value=2000,
+        strict_min=True,
+        strict_max=True,
         meta={"name": "price_range_expectation"},
+        result_format={"result_format": "COMPLETE"},
     )
