884ae9e48ce2d79c235e6a36537d8f5d8db1e4f7
dakshvanshaj <davevanshaj55646@gmail.com>
2025-06-22 21:30:52 +0530

    feat: add silver_preprocessing script containing data preprocessing functions.
    will contatin functions for silver layer data preprocessing
    like datatype conversion, duplicate removal, missing values, oulier handling and treatment etc.

---
+++ src/data_preprocessing/silver_preprocessing.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# function to optimize data types
+
+
+def optimize_data_types(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Optimize data types of a DataFrame to reduce memory usage.
+
+    Args:
+        df (pd.DataFrame): input Dataframe to optimize.
+
+    Returns:
+        pd.DataFrame: outputs a DataFrame with optimized data types.
+    """
+
+
+# Work in progress: Implement data type optimization logic
