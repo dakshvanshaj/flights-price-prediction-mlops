a32f783be4d5602dd2af76ea0d13248029e91c7e
Daksh Vanshaj <davevanshaj55646@gmail.com>
2025-07-23 16:46:19 +0000

    feat(path): add path for gold_pipeline log file.

--- src/shared/config.py
+++ src/shared/config.py
@@ -42,6 +42,7 @@ LOGGING_YAML = PROJECT_ROOT / "logging.yaml"
 BRONZE_PIPELINE_LOGS_PATH = LOGS_DIR / "bronze_pipeline.log"
 SILVER_PIPELINE_LOGS_PATH = LOGS_DIR / "silver_pipeline.log"
 SPLIT_DATA_LOGS_PATH = LOGS_DIR / "split_data.log"
+GOLD_PIPELINE_LOGS_PATH = LOGS_DIR / "gold_pipeline.log"
 
 
 # ==============================================================================
