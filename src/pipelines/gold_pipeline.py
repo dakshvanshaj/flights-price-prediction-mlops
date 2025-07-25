d1a92bbae7931700362826eb83967fd92065ccd8
Daksh Vanshaj <davevanshaj55646@gmail.com>
2025-07-25 10:57:53 +0000

    feat(WIP): add save_dataframe_based_on_validation to track changes in data.
    Add temporary success for result object to get save_dataframe_based_on_validation
    to save the dataframe in parquet format
    This way we can track the changes after steps we add in gold_pipeline.

--- src/pipelines/gold_pipeline.py
+++ src/pipelines/gold_pipeline.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 
 def gold_engineering_pipeline(
-    input_filepath: str,
+    input_filepath: Path,
 ) -> bool:
     logger.info(f"Validating file path: {input_filepath}")
     if not input_filepath.exists():
@@ -41,13 +41,15 @@ def gold_engineering_pipeline(
     df = drop_duplicates(df, keep="first")
     df = drop_missing_target_rows(df, "price")
 
-    # for testing lets say result was success
-    result = {"success": True}
+    # for testing before implementing valdiation lets say result was success
+    from types import SimpleNamespace
+
+    result = SimpleNamespace(success=True)
     # === STAGE ?: SAVE DATAFRAME BASED ON RESULT ===
     save_successful = save_dataframe_based_on_validation(
         result=result,
         df=df,
-        file_name=Path(file_name).stem,
+        file_name="test_gold",  # at the end will replace it with Path(file_name).stem
         success_dir=config.GOLD_PROCESSED_DIR,
         failure_dir=config.GOLD_QUARANTINE_DIR,
     )
@@ -57,17 +59,17 @@ def gold_engineering_pipeline(
     # === STAGE ?: LOG FINAL STATUS ===
     if pipeline_successful:
         logger.info(
-            f"--- Silver Preprocessing & Validation: PASSED for {file_name} ---"
+            f"--- GOLD Preprocessing, Feature Engineering & Validation: PASSED for {file_name} ---"
         )
     else:
         logger.warning(
-            f"--- Silver Preprocessing & Validation: FAILED for {file_name} ---"
+            f"--- GOLD Preprocessing, Feature Engineering & Validation: FAILED for {file_name} ---"
         )
 
     return pipeline_successful
 
 
-if __name__ == "__main__":
+def main():
     setup_logging_from_yaml(
         log_path=config.GOLD_PIPELINE_LOGS_PATH,
         default_level=logging.DEBUG,
@@ -85,9 +87,13 @@ if __name__ == "__main__":
     file_name = args.file_name
     input_filepath = Path(config.SILVER_PROCESSED_DIR / file_name)
 
-    pipeline_success = gold_engineering_pipeline(input_filepath=str(input_filepath))
+    pipeline_success = gold_engineering_pipeline(input_filepath=input_filepath)
 
     # Exit with a status code that an orchestrator like Airflow can interpret
     if not pipeline_success:
         sys.exit(1)
     sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
