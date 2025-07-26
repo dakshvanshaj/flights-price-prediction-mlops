fed02295043f5c66a9964b3b658c37d3d5429e05
Daksh Vanshaj <davevanshaj55646@gmail.com>
2025-07-26 17:09:29 +0000

    feat: add missing_value_imputer related configuration.

--- src/shared/config/config_gold.py
+++ src/shared/config/config_gold.py
@@ -1,4 +1,4 @@
-from shared.config.core_paths import GOLD_DATA_DIR, LOGS_DIR
+from shared.config.core_paths import GOLD_DATA_DIR, LOGS_DIR, PROJECT_ROOT
 
 # --- Gold Data Flow ---
 GOLD_PROCESSED_DIR = GOLD_DATA_DIR / "processed"
@@ -10,3 +10,9 @@ GOLD_PIPELINE_LOGS_PATH = LOGS_DIR / "gold_pipeline.log"
 # --- Gold Pipeline Config ---
 GOLD_DROP_COLS = ["travel_code", "user_code"]
 TARGET_COLUMN = "price"
+IMPUTER_STRATEGY = {
+    "median": ["price", "time", "distance"],
+    "mode": ["agency", "flight_type"],
+    "constant": {"from_location": "Unknown", "to_location": "Unknown"},
+}
+SIMPLE_IMPUTER_PATH = PROJECT_ROOT / "models" / "simple_imputer.json"
