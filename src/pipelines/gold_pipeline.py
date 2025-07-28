17661d7a2da6c5d6eb643c8e4432f62f3d818339
Daksh Vanshaj <davevanshaj55646@gmail.com>
2025-07-28 16:45:41 +0000

    feat(WIP): integrate cyclic feature creation into the pipeline.
    month, day_of_week and day will now be converted into cyclic cosine and sine
    features.

--- src/pipelines/gold_pipeline.py
+++ src/pipelines/gold_pipeline.py
@@ -11,6 +11,9 @@ from gold_data_preprocessing.data_cleaning import (
 )
 from gold_data_preprocessing.missing_value_imputer import SimpleImputer
 
+from gold_data_preprocessing.feature_engineering import create_cyclical_features
+
+
 # Import the new encoder
 from gold_data_preprocessing.categorical_encoder import CategoricalEncoder
 from typing import Optional, Tuple
@@ -21,7 +24,7 @@ logger = logging.getLogger(__name__)
 def gold_engineering_pipeline(
     input_filepath: Path,
     imputer_to_apply: Optional[SimpleImputer] = None,
-    encoder_to_apply: Optional[CategoricalEncoder] = None,
+    # encoder_to_apply: Optional[CategoricalEncoder] = None,
 ) -> Tuple[bool, Optional[SimpleImputer], Optional[CategoricalEncoder]]:
     """
     Executes the full gold layer pipeline: cleaning, imputation, and encoding.
@@ -57,19 +60,25 @@ def gold_engineering_pipeline(
         df = imputer.fit_transform(df)
         fitted_imputer = imputer  # This is the new imputer we just trained
 
+    # === STAGE 4: CYCLIC FEATURE ENCODING ===
+    logger.info("=" * 25 + " STAGE 4/5: CYCLIC FEATURE ENCODING " + "=" * 25)
+    df = create_cyclical_features(df, cyclical_map=config_gold.CYCLICAL_MAP)
+
+    # === STAGE 5: FEATURE CREATION ===
+
     # === STAGE 4: ENCODE CATEGORICAL COLUMNS (TRAINING OR INFERENCE) ===
-    logger.info("=" * 25 + " STAGE 4/5: ENCODING " + "=" * 25)
-    fitted_encoder = None
-    if encoder_to_apply:
-        # --- INFERENCE MODE ---
-        logger.info("Applying existing encoder...")
-        df = encoder_to_apply.transform(df)
-    else:
-        # --- TRAINING MODE ---
-        logger.info("Fitting new encoder...")
-        encoder = CategoricalEncoder(encoding_config=config_gold.ENCODING_CONFIG)
-        df = encoder.fit_transform(df)
-        fitted_encoder = encoder  # This is the new encoder we just trained
+    # logger.info("=" * 25 + " STAGE 4/5: ENCODING " + "=" * 25)
+    # fitted_encoder = None
+    # if encoder_to_apply:
+    #     # --- INFERENCE MODE ---
+    #     logger.info("Applying existing encoder...")
+    #     df = encoder_to_apply.transform(df)
+    # else:
+    #     # --- TRAINING MODE ---
+    #     logger.info("Fitting new encoder...")
+    #     encoder = CategoricalEncoder(encoding_config=config_gold.ENCODING_CONFIG)
+    #     df = encoder.fit_transform(df)
+    #     fitted_encoder = encoder  # This is the new encoder we just trained
 
     # === STAGE 5: SAVE DATAFRAME ===
     logger.info("=" * 25 + " STAGE 5/5: SAVE DATAFRAME " + "=" * 25)
@@ -91,7 +100,7 @@ def gold_engineering_pipeline(
     else:
         logger.warning(f"--- Gold Pipeline: FAILED for {file_name} ---")
 
-    return pipeline_successful, fitted_imputer, fitted_encoder
+    return pipeline_successful, fitted_imputer  # , fitted_encoder
 
 
 def main():
@@ -103,22 +112,22 @@ def main():
 
     # --- ORCHESTRATION LOGIC ---
     imputer_path = config_gold.SIMPLE_IMPUTER_PATH
-    encoder_path = config_gold.CATEGORICAL_ENCODER_PATH
+    # encoder_path = config_gold.CATEGORICAL_ENCODER_PATH
 
     # 1. Process Training Data
     logger.info(">>> ORCHESTRATOR: Processing training data...")
     train_path = config_silver.SILVER_PROCESSED_DIR / "train.parquet"
-    train_success, fitted_imputer, fitted_encoder = gold_engineering_pipeline(
+    train_success, fitted_imputer = gold_engineering_pipeline(
         input_filepath=train_path
-    )
+    )  # re add fitted_encoder once feature engineering is done
 
-    if not train_success or not fitted_imputer or not fitted_encoder:
+    if not train_success or not fitted_imputer:  # or not fitted_encoder:
         logger.critical("Training data processing failed. Aborting.")
         sys.exit(1)
 
     # Save the fitted preprocessing objects
     fitted_imputer.save(imputer_path)
-    fitted_encoder.save(encoder_path)
+    # fitted_encoder.save(encoder_path)
     logger.info("Successfully fitted and saved imputer and encoder.")
 
     # 2. Process Validation Data
@@ -126,11 +135,11 @@ def main():
     validation_path = config_silver.SILVER_PROCESSED_DIR / "validation.parquet"
     # Load the objects that were fitted on the training data
     loaded_imputer = SimpleImputer.load(imputer_path)
-    loaded_encoder = CategoricalEncoder.load(encoder_path)
-    validation_success, _, _ = gold_engineering_pipeline(
+    # loaded_encoder = CategoricalEncoder.load(encoder_path)
+    validation_success, _ = gold_engineering_pipeline(
         input_filepath=validation_path,
         imputer_to_apply=loaded_imputer,
-        encoder_to_apply=loaded_encoder,
+        # encoder_to_apply=loaded_encoder,
     )
     if not validation_success:
         logger.error("Validation data processing failed.")
@@ -140,11 +149,11 @@ def main():
     test_path = config_silver.SILVER_PROCESSED_DIR / "test.parquet"
     # Re-load for safety, though the objects are the same
     loaded_imputer = SimpleImputer.load(imputer_path)
-    loaded_encoder = CategoricalEncoder.load(encoder_path)
-    test_success, _, _ = gold_engineering_pipeline(
+    # loaded_encoder = CategoricalEncoder.load(encoder_path)
+    test_success, _ = gold_engineering_pipeline(
         input_filepath=test_path,
         imputer_to_apply=loaded_imputer,
-        encoder_to_apply=loaded_encoder,
+        # encoder_to_apply=loaded_encoder,
     )
     if not test_success:
         logger.error("Test data processing failed.")
