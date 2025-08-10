4502a877c24c310e9a3fb6ed46b2bd74c88a6a93
Daksh Vanshaj <davevanshaj55646@gmail.com>
2025-08-10 09:22:16 +0000

    feat: use python operator for triggering bronze, silver and gold pipeline separately.

--- airflow/dags/data_pipeline_dag.py
+++ airflow/dags/data_pipeline_dag.py
@@ -1,6 +1,11 @@
 from airflow.models.dag import DAG
 import pendulum
-from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+# Import your pipeline functions
+from pipelines.bronze_pipeline import main as run_bronze_pipeline
+from pipelines.gold_pipeline import main as run_gold_pipeline
+from pipelines.silver_pipeline import main as run_silver_pipeline
 
 default_args = {
     "owner": "Daksh",
@@ -9,17 +14,28 @@ default_args = {
 }
 
 with DAG(
-    dag_id="dvc_repro_pipeline",
-    start_date=pendulum.datetime(2024, 8, 1, tz="UTC"),
-    description="This DAG runs the main DVC pipeline using 'dvc repro'.",
+    dag_id="data_pipeline_v1",
+    start_date=pendulum.datetime(2025, 8, 1, tz="UTC"),
+    description="This dag runs individual Bronze, Silver and Gold pipelines.",
     schedule=None,
     catchup=False,
-    tags=["flights_mlops", "dvc", "bash"],
+    tags=["data_pipeline", "pipeline", "bronze", "silver", "gold"],
     default_args=default_args,
 ) as dag:
-    # This task now correctly changes directory to the project root
-    # inside the container before running the dvc command.
-    run_dvc_pipeline = BashOperator(
-        task_id="run_dvc_repro",
-        bash_command="cd /opt/airflow/Flights_Mlops && dvc repro",
+    # Task 1: Bronze Layer
+    bronze_pipeline = PythonOperator(
+        task_id="run_bronze_pipeline_and_validate", python_callable=run_bronze_pipeline
     )
+
+    # Task 2: Silver Layer
+    silver_pipeline = PythonOperator(
+        task_id="run_silver_pipeline_and_validate", python_callable=run_silver_pipeline
+    )
+
+    # Task 3: Gold Layer
+    gold_pipeline = PythonOperator(
+        task_id="run_gold_pipeline_and_validate", python_callable=run_gold_pipeline
+    )
+
+    # Set dependencies using the task objects  created
+    bronze_pipeline >> silver_pipeline >> gold_pipeline
