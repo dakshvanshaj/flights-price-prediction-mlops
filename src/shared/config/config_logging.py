192963cb12644de204d2e094959fadf40196c2f0
Daksh Vanshaj <davevanshaj55646@gmail.com>
2025-09-15 16:20:10 +0000

    chore: add MAIN_FAST_API_LOGS_PATH.

--- src/shared/config/config_logging.py
+++ src/shared/config/config_logging.py
@@ -8,3 +8,5 @@ SPLIT_DATA_LOGS_PATH = LOGS_DIR / "split_data.log"
 GOLD_PIPELINE_LOGS_PATH = LOGS_DIR / "gold_pipeline.log"
 TRAINING_PIPELINE_LOGS_PATH = LOGS_DIR / "training_pipeline.log"
 TUNING_PIPELINE_LOGS_PATH = LOGS_DIR / "tuning_pipeline.log"
+
+MAIN_FAST_API_LOGS_PATH = LOGS_DIR / "main_fastapi.log"
